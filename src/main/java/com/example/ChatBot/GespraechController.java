3b2383f9029ca7c2afd8e9e41b4a18ab3035dba3
ecoskun <eyuep.coskun@adesso.de>
2023-08-31 13:35:28 +0200

    GespraechController class erstellt, wo post request empfÃängt wird

---
+++ src/main/java/com/example/ChatBot/GespraechController.java
@@ -0,0 +1,10 @@
+package com.example.ChatBot;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GespraechController {
+
+    // hier  nur post methode für Gespräch zu implementieren.
+    //
+}
